7ad9ca3ba26a6b6f856ecee39c67e59b166e6544
samescolas <samescolas@gmail.com>
2017-08-02 15:46:27 -0700

    Testing to make sure l prints different than regs.

--- main.c
+++ main.c
@@ -5,6 +5,11 @@ void	ft_printf(char *fmt, ... );
 
 int		main(void)
 {
-	ft_printf("%-3.5d\n", 42);
+	ft_printf("%-3.5d\n", 3000000000);
+	printf("%-3.5ld\n", 3000000000);
+
+	ft_printf("%%\n");
+
+	ft_printf("This string contains %ld numbers and %03d other things.?", 3000000000, 17);
 	return (0);
 }
