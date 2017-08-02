08227784fcf5d86bd61f149f1799dbc5cc6ec875
Sam Escolas <sescolas@student.42.us.org>
2017-08-01 22:30:01 -0400

    Inclusive test case

--- main.c
+++ main.c
@@ -5,13 +5,6 @@ void	ft_printf(char *fmt, ... );
 
 int		main(void)
 {
-	ft_printf("ME: octal num: %o\n", 42);
-	printf("XX: octal num: %o\n\n", 42);
-
-	ft_printf("ME: hex num: %x\n", 42);
-	printf("XX: hex num: %x\n\n", 42);
-
-	ft_printf("ME: long hex num: %lx\n", 174217421742);
-	printf("XX: long hex num: %lx\n", 174217421742);
+	ft_printf("%-3.5d\n", 42);
 	return (0);
 }
