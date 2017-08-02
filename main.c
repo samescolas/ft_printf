62ba7915584c29395e0f2daa5f16a83aaafb1582
Sam Escolas <sescolas@student.42.us.org>
2017-08-01 21:44:06 -0400

    IDk

--- main.c
+++ main.c
@@ -7,8 +7,10 @@ int		main(void)
 {
 	ft_printf("ME: octal num: %o\n", 42);
 	printf("XX: octal num: %o\n\n", 42);
+
 	ft_printf("ME: hex num: %x\n", 42);
 	printf("XX: hex num: %x\n\n", 42);
+
 	ft_printf("ME: long hex num: %lx\n", 174217421742);
 	printf("XX: long hex num: %lx\n", 174217421742);
 	return (0);
